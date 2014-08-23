81494a0690cd7cd6b897c2d167132dc75476032f
mark rubenstein & Dylan Peerenboom <student@epicodus.com>
2014-08-22 17:16:18 -0700

    add js for nav collapse

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,15 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-= require jquery
-= require bootstrap-sprockets
+//= require bootstrap-sprockets
+
+(function() {
+    var nav = document.getElementById('nav'),
+        anchor = nav.getElementsByTagName('a'),
+        current = window.location.pathname.split('/')[1];
+        for (var i = 0; i < anchor.length; i++) {
+        if(anchor[i].href == current) {
+            anchor[i].className = "active";
+        }
+    }
+})();
